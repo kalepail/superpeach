bee3235924d058a1c9c132a53375c7c286e4a81d
Tyler van der Hoeven <hi@tyvdh.com>
2024-05-16 21:38:36 -0400

    huh

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,22 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import svelte from "@astrojs/svelte";
-
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  adapter: cloudflare({
+    imageService: 'passthrough',
+    platformProxy: {
+      enabled: true,
+      persist: true,
+    }
+  }),
   vite: {
     plugins: [nodePolyfills({
       include: ['buffer']
     })]
   },
   integrations: [tailwind(), svelte()],
-  output: "server",
-  adapter: cloudflare()
 });
