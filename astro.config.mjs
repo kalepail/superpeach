a0671335f56349f3bc0fa5d847f097094d9d0c81
Tyler van der Hoeven <hi@tyvdh.com>
2024-06-14 17:06:42 -0400

    enable analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,9 @@ export default defineConfig({
     output: "server",
     adapter: vercel({
         excludeFiles: ["site/**/*"],
+        webAnalytics: {
+            enabled: true,
+        }
     }),
     // vite: {
     //     plugins: [
