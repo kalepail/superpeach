545f9851fbdaae15f9e5eae1b53d630b3bf6c6ad
Tyler van der Hoeven <hi@tyvdh.com>
2024-08-29 10:50:18 -0400

    setMercuryJwt isn't supported anymore

--- site/src/pages/api/contract-id/[signer].ts
+++ site/src/pages/api/contract-id/[signer].ts
@@ -2,8 +2,6 @@ import type { APIRoute } from "astro"
 import { account } from "../../../lib/common-server";
 
 export const GET: APIRoute = async ({ params }) => {
-    await account.setMercuryJwt()
-
     const contractId = await account.getContractId(params.signer!)
 
     if (!contractId)
--- src/pages/api/signers/[contractId].ts
+++ src/pages/api/signers/[contractId].ts
@@ -2,9 +2,7 @@ import type { APIRoute } from "astro"
 import { account } from "../../../lib/common-server";
 
 export const GET: APIRoute = async ({ params }) => {
-    await account.setMercuryJwt()
-
     const signers = await account.getSigners(params.contractId!)
-
+    
     return Response.json(signers)
 }
