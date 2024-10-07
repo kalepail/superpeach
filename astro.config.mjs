09830864d458de36fc8b75e7cd5b6b4e1b95c1f1
Tyler van der Hoeven <hi@tyvdh.com>
2024-10-07 11:03:47 -0400

    try

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import cloudflare from '@astrojs/cloudflare';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,6 +12,11 @@ export default defineConfig({
         define: {
             "process.env": process.env
         },
+        plugins: [
+            nodePolyfills({
+                include: ['buffer']
+            })
+        ]
     },
     integrations: [tailwind(), svelte()]
 });
